14f8c9bed650f4d5f84fe0967bb0b2b75f1423db
samuFerragut <sferragut@cifpfbmoll.eu>
2020-05-17 17:24:31 +0200

    feat: Añadir Javadoc al proyecto

--- src/main/java/edu/elsmancs/domain/Receptivo.java
+++ src/main/java/edu/elsmancs/domain/Receptivo.java
@@ -1,16 +1,33 @@
 package edu.elsmancs.domain;
 
-public class Receptivo implements GuestDispatcher {
+import java.util.ArrayList;
+import java.util.List;
+        /**
+        * La clase receptivo sirve como un metodo global para registrar y despachar todos los productos en un pack
+        *
+        * Aprovechamos la implementación de la interfaz guestDispatcher para generalizar todos los metodos dispatch y
+        * que puedan ser usados en un mismo metodo.
+        *
+        * @author Samu Ferragut
+        */
+public class Receptivo {
+
+    List<GuestDispatcher> dispatches = new ArrayList<>();
 
     public Receptivo(){
     };
-
+    /**
+    * Metodo para añadir en la lista de metodos dispatch, uno nuevo.
+    * @param pack Nuevo elemento que queremos añadir a dispatches
+    */
     public void registra(GuestDispatcher pack) {
-
+        this.dispatches.add(pack);
     }
-
-    @Override
-    public void dispatch(CreditCard tarjeta) {
-
+    /**
+    * Por cada uno de los dispatches guardados en la List, se aplicara al user dado
+    *  @param user el objeto CreditCard que le pasamos que usamos como identificativo para cada usuario
+    */
+    public void dispatch(CreditCard user) {
+        dispatches.forEach( type -> type.dispatch(user)) ;
     }
 }
